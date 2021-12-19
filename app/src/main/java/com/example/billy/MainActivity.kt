dffcce6946725019dca8459e7f976f1a69b76128
Воробьев П.А. (Таб. номер 561873653) <pa.vorobev1@tensor.ru>
2021-12-19 14:13:51 +0300

    Обновлёна MainActivity Переведены все элементы окна на русский + комментарии к коду

--- app/src/main/java/com/example/billy/MainActivity.kt
+++ app/src/main/java/com/example/billy/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.billy
 
 import android.content.Intent
+import android.graphics.PointF
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -8,31 +9,34 @@ import android.widget.Toast
 import androidx.appcompat.widget.AppCompatEditText
 
 class MainActivity : AppCompatActivity() {
-
-    // Список элементов на поле. Достучаться по ID не получается, поэтому
-    // используется внутренняя функция findViewById()
-    private var sStartButton: Button = findViewById(R.id.main_activity_start_button)
-    private var sNameEdit: AppCompatEditText = findViewById(R.id.main_activity_name_edit)
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Получение элементов поля
+        val sStartButton: Button = findViewById(R.id.main_activity_start_button)
+        val sNameEdit: AppCompatEditText = findViewById(R.id.main_activity_name_edit)
+
         // Обработка поведения при нажатии на кнопку Start
-        sStartButton!!.setOnClickListener {
-            if (sNameEdit!!.text.toString().isEmpty()){
-                Toast.makeText(this, "Введи своё имя, ♂slave♂", Toast.LENGTH_SHORT).show()
-            } else {
-                // Переход на другое Activity, если имя прошло все проверки
-                val intent = Intent(this, QuestionActivity::class.java)
-                // Проброс полей для Activity с результатом
-                intent.putExtra(Constants.USER_NAME, sNameEdit!!.text.toString())
+        sStartButton.setOnClickListener {
+            val insertedName: String = sNameEdit.text.toString()
+            when {
+                insertedName.isEmpty() -> {
+                    Toast.makeText(this, "Введи своё имя, ♂slave♂", Toast.LENGTH_SHORT).show()
+                }
+                insertedName.length <= 3 -> {
+                    Toast.makeText(this, "Имя слишком короткое, ♂slave♂", Toast.LENGTH_SHORT).show()
+                }
+                else -> {
+                    // Переход на другое Activity, если имя прошло все проверки
+                    val intent = Intent(this, QuestionActivity::class.java)
+                    // Проброс полей для Activity с результатом
+                    intent.putExtra(Constants.USER_NAME, sNameEdit.text.toString())
+                    startActivity(intent)
+
+                    finish()
+                }
             }
         }
-    }
-
-    private fun initializeFields(){
-        sStartButton = findViewById(R.id.main_activity_start_button)
-        sNameEdit = findViewById(R.id.main_activity_name_edit)
     }
 }
