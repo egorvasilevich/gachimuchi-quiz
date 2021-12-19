d31021c8521e43887e0387ed808f25322cb0cfc1
Воробьев П.А. (Таб. номер 561873653) <pa.vorobev1@tensor.ru>
2021-12-19 23:03:58 +0300

    Добавлено поле для выбора количества вопросов Добавлены насмешки при неверных данных

--- app/src/main/java/com/example/billy/MainActivity.kt
+++ app/src/main/java/com/example/billy/MainActivity.kt
@@ -17,6 +17,7 @@ class MainActivity : AppCompatActivity() {
         // Получение элементов поля
         val sStartButton: Button = findViewById(R.id.main_activity_start_button)
         val sNameEdit: AppCompatEditText = findViewById(R.id.main_activity_name_edit)
+        val sNumberOfQuestionsEdit: AppCompatEditText = findViewById(R.id.main_activity_number_of_questions)
 
         // Установка прошлого имени пользователя, если мы пришли с финального экрана
         val previousUsername = intent.getStringExtra(Constants.USER_NAME)
@@ -30,23 +31,44 @@ class MainActivity : AppCompatActivity() {
 
         // Обработка поведения при нажатии на кнопку Start
         sStartButton.setOnClickListener {
+
+            var nameChecked = false
+            var numberOfQuestionsChecked = false
+
+            // Проверка имени
             val insertedName: String = sNameEdit.text.toString()
             when {
                 insertedName.isEmpty() -> {
-                    Toast.makeText(this, "Введи своё имя, ♂slave♂", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(this, Constants.UNDEFINED_NAME_TAUNTS[Random.nextInt(0, Constants.UNDEFINED_NAME_TAUNTS.size)], Toast.LENGTH_SHORT).show()
                 }
-                insertedName.length <= 3 -> {
-                    Toast.makeText(this, "Имя слишком короткое, ♂slave♂", Toast.LENGTH_SHORT).show()
+                insertedName.length < 2 -> {
+                    Toast.makeText(this, Constants.SHORT_NAME_TAUNTS[Random.nextInt(0, Constants.SHORT_NAME_TAUNTS.size-1)], Toast.LENGTH_SHORT).show()
                 }
                 else -> {
-                    // Переход на другое Activity, если имя прошло все проверки
-                    val intent = Intent(this, QuestionActivity::class.java)
-                    // Проброс полей для Activity с результатом
-                    intent.putExtra(Constants.USER_NAME, sNameEdit.text.toString())
-                    startActivity(intent)
-
-//                    finish()
+                    nameChecked = true
                 }
+            }
+
+            // Проверка количества вопросов
+            val insertedNumberOfQuestions: String = sNumberOfQuestionsEdit.text.toString()
+            when  (val numberOfQuestions: Int? = insertedNumberOfQuestions.toIntOrNull()){
+                null -> {
+                    Toast.makeText(this, Constants.INCORRECT_NUMBER_OF_QUESTIONS_TAUNTS[Random.nextInt(0,Constants.INCORRECT_NUMBER_OF_QUESTIONS_TAUNTS.size)], Toast.LENGTH_SHORT).show()
+                }
+                else -> {
+                    if (numberOfQuestions < 3 || numberOfQuestions > Constants.TOTAL_QUESTIONS.length){
+                        Toast.makeText(this, Constants.INCORRECT_NUMBER_OF_QUESTIONS_TAUNTS[Random.nextInt(0,Constants.INCORRECT_NUMBER_OF_QUESTIONS_TAUNTS.size)], Toast.LENGTH_SHORT).show()
+                    } else {
+                        numberOfQuestionsChecked = true
+                    }
+                }
+            }
+            if (numberOfQuestionsChecked && nameChecked){
+                val intent = Intent(this, QuestionActivity::class.java)
+                intent.putExtra(Constants.USER_NAME, sNameEdit.text.toString())
+                intent.putExtra(Constants.QUESTIONS_COUNT, sNumberOfQuestionsEdit.text.toString().toInt())
+                startActivity(intent)
+                finish()
             }
         }
     }
